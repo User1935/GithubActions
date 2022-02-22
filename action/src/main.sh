21c9955db4f659145850a38158dace143712e6e4
User1935 <70213909+User1935@users.noreply.github.com>
2022-02-22 23:50:37 +0000

    Update main.sh

--- action/src/main.sh
+++ action/src/main.sh
@@ -116,12 +116,7 @@ function installTerraform {
 }
 
 function installAZ {
-  sudo apt-get install ca-certificates curl apt-transport-https lsb-release gnupg
-  curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
-  AZ_REPO=$(lsb_release -cs)
-  echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
-  sudo apt-get update
-  sudo apt-get install azure-cli
+  az login --service-principal --username $1 --password $3 --tenant $2
 }
 
 function installTerragrunt {
