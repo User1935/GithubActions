f93249c9ef7df25b14c93aa3342699cd12bdd3c5
User1935 <70213909+User1935@users.noreply.github.com>
2022-02-23 12:52:56 +0000

    Update main.sh

--- action/src/main.sh
+++ action/src/main.sh
@@ -116,7 +116,7 @@ function installTerraform {
 }
 
 function installAZ {
-  az login --service-principal --username ${1}[0] --password ${1}[2] --tenant ${1}[1]
+  az login --service-principal --username ${1} --password ${3} --tenant ${2}
 }
 
 function installTerragrunt {
@@ -164,7 +164,7 @@ function main {
   source ${scriptDir}/terragrunt_taint.sh
   source ${scriptDir}/terragrunt_destroy.sh
   
-  echo ARG1: ${1}[0] ARG2: ${1}[1] ARG3: ${1}[2]
+  echo ARG1: ${1} ARG2: ${1} ARG3: ${1}
   installAZ
   parseInputs
   configureCLICredentials
