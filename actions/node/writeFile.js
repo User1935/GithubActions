c7fa8bec0a31dc77367c9c8ce3ed4b738e871075
Luis Archer <luis.archer@ritain.io>
2022-03-10 12:33:16 +0000

    Modifications done

--- actions/node/writeFile.js
+++ actions/node/writeFile.js
@@ -13,4 +13,4 @@ fs.writeFile("/tmp/test", process.env.FILE_OUTPUT , function(err) {
 //let buff = new Buffer.from(process.env.FILE_INPUT, 'base64');
 //let text = buff.toString('utf8');
 // changed FIlE_OUPUT TO INPUT
-fs.writeFileSync(process.env.FILE_INPUT, text);
+fs.writeFileSync(process.env.FILE_OUTPUT, process.env.FILE_INPUT);
