8aa15f8ad05f66f6b6dec57cefffe424ea7a8494
Luis Archer <luis.archer@ritain.io>
2022-03-14 11:50:19 +0000

    Modifications done

--- actions/pre-commit/src/main.sh
+++ actions/pre-commit/src/main.sh
@@ -7,7 +7,7 @@ function main {
   echo '    - id: terraform_validate' >> .pre-commit-config.yaml
   fi
   #  sed 's/\x1b\[[0-9;]*m//g'
-  pre-commit run -a | sed 's/\x1b\[[0-9;]*m//g' | tee comment_pre-commit.md
+  pre-commit run -a | tee comment_pre-commit.md
   #echo Character Type $LC_CTYPE Language $LANG
   #base64 comment_pre-commit.md > b64encfile.md
   #echo ::set-output name=filecontent::$(cat b64encfile.md)
