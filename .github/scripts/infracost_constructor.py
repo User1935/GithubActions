c5d53f151f0c7411d98dbc86743e2e756da5c69d
Luis Archer <luis.archer@ritain.io>
2022-03-31 17:02:34 +0100

    Modifications done

--- .github/scripts/infracost_constructor.py
+++ .github/scripts/infracost_constructor.py
@@ -6,7 +6,7 @@ data = sys.argv[1]
 decdata = json.loads(data)
 filecontent = ""
 for ele in decdata['include']:
-    filecontent += "\t- path: " + ele['path'][:-1] + "\n"
+    filecontent += "    - path: " + ele['path'][:-1] + "\n"
 yaml = '''
 version: 0.1
 
